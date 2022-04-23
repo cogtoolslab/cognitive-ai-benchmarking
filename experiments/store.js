6fde15961116ab214777c5156a8639bfbcf65a2e
Felix Binder <me@felixbinder.net>
2022-04-23 17:52:01 +0000

    better logging

--- experiments/store.js
+++ experiments/store.js
@@ -72,7 +72,7 @@ function serve() {
       if (!request.body) {
         return failure(response, '/db/insert needs post request body');
       }
-      console.log(`got request to insert into ${request.body.colname}`);
+      console.log(`got request to insert into ${request.body.dbname}, ${request.body.colname}`);
 
       const databaseName = request.body.dbname;
       const collectionName = request.body.colname;
