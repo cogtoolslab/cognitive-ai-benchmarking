be668029aa98d0f272cb707745cecab4ee2c471f
Felix Binder <me@felixbinder.net>
2023-02-10 15:12:00 -0800

    reworked the database download code

--- analysis/generate_dataframes.py
+++ analysis/generate_dataframes.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import argparse
 
 os.getcwd()
 sys.path.append("..")
@@ -23,6 +24,8 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 
+REPO = 'cognitive-ai-benchmarking'
+
 #helper function for pd.agg
 def item(x):
     """Returns representative single item"""
@@ -30,18 +33,27 @@ def item(x):
 
 # set up directories
 ## directory & file hierarchy
-try:
-    proj_dir = str(os.path.abspath(__file__)).split('physics-benchmarking-neurips2021')[0]+'physics-benchmarking-neurips2021'
-except:
-    print("ERROR: this script needs to be located in the human-physics-benchmarking folder")
-analysis_dir =  os.path.join(proj_dir,'analysis')
-results_dir = os.path.join(proj_dir,'results')
-csv_dir = os.path.join(results_dir,'csv/humans')
+# get path of this file
+current_dir = os.path.dirname(os.path.abspath(__file__))
+while current_dir != '/':
+    try:
+        if REPO in os.listdir(current_dir):
+            proj_dir = os.path.join(current_dir, REPO)
+            break
+        else:
+            current_dir = os.path.dirname(current_dir)
+    except:
+        print("Could not find cognitive-AI-benchmarking repo. Please make sure it is in the a parent directory as this repo under the name `cognitive-AI-benchmarking`. Edit this file if you want to change the name of the repo.")
+        sys.exit()
+if current_dir == '/': 
+    print("Could not find cognitive-AI-benchmarking repo. Please make sure it is in the a parent directory as this repo under the name `cognitive-AI-benchmarking`. Edit this file if you want to change the name of the repo.")
+    sys.exit()
 
 ## add helpers to python path
-if os.path.join(proj_dir,'stimuli') not in sys.path:
-    sys.path.append(os.path.join(proj_dir,'stimuli'))
-    
+
+results_dir = os.path.join(proj_dir, 'results')
+csv_dir = os.path.join(proj_dir, 'csv')
+
 if not os.path.exists(results_dir):
     os.makedirs(results_dir)
     
@@ -49,8 +61,17 @@ if not os.path.exists(csv_dir):
     os.makedirs(csv_dir)       
     
 ## add helpers to python path
+sys.path.append(proj_dir)
+
 if os.path.join(proj_dir,'utils') not in sys.path:
     sys.path.append(os.path.join(proj_dir,'utils'))   
+
+if os.path.join(proj_dir,'stimuli') not in sys.path:
+    sys.path.append(os.path.join(proj_dir,'stimuli'))   
+
+# CAB imports
+import cabutils
+from stimuli.experiment_config import *
 
 def make_dir_if_not_exists(dir_name):   
     if not os.path.exists(dir_name):
@@ -72,71 +93,23 @@ def anonymize(subjID):
 ## create directories that don't already exist        
 result = [make_dir_if_not_exists(x) for x in [results_dir,csv_dir]]
 
-# set vars 
-try:
-    auth = pd.read_csv(os.path.join(analysis_dir,'auth.txt'), header = None) # this auth.txt file contains the password for the sketchloop user. Place in repo folder
-except: 
-    print('ERROR: Before you can generate dataframes, please make sure you have the auth.txt file with mongodb credentials.')
-    sys.exit()
-
-pswd = auth.values[0][0]
-user = 'sketchloop'
-host = 'cogtoolslab.org'
-
-# do we want to anonymize prolific IDs?
-anonymizeIDs=True
-
 # have to fix this to be able to analyze from local
 import pymongo as pm
 try:
-    conn = pm.MongoClient('mongodb://sketchloop:' + pswd + '@127.0.0.1')
+    conn = get_db_connection()
+    print('Connected to database.')
 except:
     print('Could not connect to database. Try to set up ssh bridge to write to mongodb. Insert your username. If you dont have an SSH secret set yet, run `ssh -fNL 27017:127.0.0.1:27017 USERNAME@cogtoolslab.org` in your shell.')
     sys.exit()
 
-neurips2021_iterations = [
-    {'study' : "dominoes_pilot",
-    'bucket_name' : 'human-physics-benchmarking-dominoes-pilot',
-    'stim_version' : 'production_1',
-    'iterationName' : 'production_1_testing'},
-    {'study' : "collision_pilot",
-    'bucket_name' : 'human-physics-benchmarking-collision-pilot',
-    'stim_version' : 'production_2',
-    'iterationName' : 'production_2_testing'},
-    {'study' : "towers_pilot",
-    'bucket_name' : 'human-physics-benchmarking-towers-pilot',
-    'stim_version' : 'production_2',
-    'iterationName' : 'production_2_testing'},
-    {'study' : "linking_pilot",
-    'bucket_name' : 'human-physics-benchmarking-linking-pilot',
-    'stim_version' : 'production_2',
-    'iterationName' : 'production_2_testing'},
-    {'study' : "containment_pilot",
-    'bucket_name' : 'human-physics-benchmarking-containment-pilot',
-    'stim_version' : 'production_2',
-    'iterationName' : 'production_2_testing'},
-    {'study' : "rollingsliding_pilot",
-    'bucket_name' : 'human-physics-benchmarking-rollingsliding-pilot',
-    'stim_version' : 'production_2',
-    'iterationName' : 'production_2_testing'},
-    {'study' : "drop_pilot",
-    'bucket_name' : 'human-physics-benchmarking-drop-pilot',
-    'stim_version' : 'production_2',
-    'iterationName' : 'production_2_testing'},
-    {'study' : "clothiness_pilot",
-    'bucket_name' : 'human-physics-benchmarking-clothiness-pilot',
-    'stim_version' : 'production_2',
-    'iterationName' : 'production_2_testing'},
-]
-
-def get_dfs_from_mongo(study,bucket_name,stim_version,iterationName):
+def get_dfs_from_mongo(project, dataset, task, iteration, anonymizeIDs=True):
     """Get's and saves the given iteration from the mongoDB. Writes out two dataframes."""
-    df_trial_entries, df_familiarization_entries = pull_dataframes_from_mongo(study, bucket_name, stim_version, iterationName)
+    df_trial_entries, df_familiarization_entries = pull_dataframes_from_mongo(project, dataset, task, iteration, anonymizeIDs)
 
     # save out df_trials_entries
-    df_trial_entries.to_csv(os.path.join(csv_dir,"human_responses-{}-{}.csv".format(study,iterationName)))
+    df_trial_entries.to_csv(os.path.join(csv_dir,"human_responses_{}_{}_{}.csv".format(project, dataset++task, iteration)))
     # save out df_famili arizations_entries
-    df_familiarization_entries.to_csv(os.path.join(csv_dir,"familiarization_human_responses-{}-{}.csv".format(study,iterationName)))
+    df_familiarization_entries.to_csv(os.path.join(csv_dir,"familiarization_human_responses_{}_{}_{}.csv".format(project, dataset++task, iteration)))
 
     #generate per stim aggregated df
     df_trial_entries['c'] = 1 #add dummy variable for count in agg
@@ -145,24 +118,27 @@ def get_dfs_from_mongo(study,bucket_name,stim_version,iterationName):
         'c' : 'count',
     })
     #save
-    per_stim_agg.to_csv(os.path.join(csv_dir,"human_accuracy-{}-{}.csv".format(study,iterationName)))
+    per_stim_agg.to_csv(os.path.join(csv_dir,"human_accuracy_{}_{}_{}.csv".format(project, dataset++task, iteration)))
     return
 
-
-def pull_dataframes_from_mongo(study, bucket_name, stim_version, iterationName, database_name='human_physics_benchmarking'):
+def pull_dataframes_from_mongo(project, dataset, task, iteration, anonymizeIDs=True):
     """Gets dataframes from mongo and returns both the experimental and the familiarization trials"""
     # connect to database
-    db = conn[database_name]
-    coll = db[study]
-    stim_db = conn['stimuli']
-    stim_coll = stim_db[bucket_name+'_'+stim_version]
+    experiment = dataset+'_'+task
+    db = conn[project+'_output']
+    coll = db[experiment]
+    stim_db = conn[project+'_input']
+    stim_coll = stim_db[experiment]
 
+    print('Reading from {}_output.{} and {}_input.{}'.format(project, experiment, project, experiment))
+    print('Reading iteration {}'.format(iteration))
+    print("This might take a while...")
     # get dataframe of served stims
     stim_df = pd.DataFrame(stim_coll.find({}))
     stim_df.set_index('_id')
     df = coll.find({
-            'iterationName':iterationName,
-            'prolificID': {'$exists' : True},
+            'iterationName': iteration,
+            # 'prolificID': {'$exists' : True},
             'studyID': {'$exists' : True},
             'sessionID': {'$exists' : True},
     })
@@ -170,36 +146,52 @@ def pull_dataframes_from_mongo(study, bucket_name, stim_version, iterationName, 
     
     assert len(df)>0, "df from mongo empty"
 
-    #Which gameids have completed all trials that were served to them? 
-    #Note that this will also exclude complete trials whose games aren't in the stim database anymore (ie if it has been dropped)
+    print("Got {} rows from the database".format(len(df)))
+
+    pID_count = df['prolificID'].nunique()
+    df = fill_ProlificIDs(df)
+    if df['prolificID'].nunique() > pID_count: print("Filled {} Prolific IDs from manually recorded ones".format(df['prolificID'].nunique() - pID_count))
+    # Which gameids have completed all trials that were served to them? 
+    # Note that this will also exclude complete trials whose games aren't in the stim database anymore (ie if it has been dropped)
+
     complete_gameids = []
     for gameid in tqdm(df['gameID'].unique()):
-        #get the corresponding games
-        served_stim_ID = None
-        for stims_ID in stim_df.index:
+        # if participants have given a sex, they have completed the experiment
+        # TODO generalize this to other tasks
+        responses = df[df['gameID'] == gameid]['responses']
+        for response in list(responses):
             try:
-                if gameid in stim_df.iloc[stims_ID]['games']:
-                    #great, we found our corresponding stim_ID
-                    served_stim_ID = stims_ID
+                if 'participantSex' in response:
+                    complete_gameids.append(gameid)
+                    break
             except TypeError as e:
-    #             print("No games listed for",stims_ID)
-                    pass
-        if served_stim_ID == None:
-            #we haven't found the stim_ID
-            print("No recorded entry for game_ID in stimulus database:",gameid)
-            continue
-        served_stims = stim_df.at[served_stim_ID,'stims']
-        #let's check if we can find an entry for each stim
-        found_empty = False
-        for stim_ID in [s['stim_ID'] for s in served_stims.values()]:
-            #check if we have an entry for that stimulus
-            if len(df.query("gameID == '"+gameid+"' & stim_ID == '"+stim_ID+"'")) == 0:
-                found_empty = True
-                break
-        if not found_empty: complete_gameids.append(gameid)
+                pass
+        #get the corresponding games
+        # served_stim_ID = None
+        # for stims_ID in stim_df.index:
+        #     try:
+        #         if gameid in stim_df.iloc[stims_ID]['games']:
+        #             #great, we found our corresponding stim_ID
+        #             served_stim_ID = stims_ID
+        #     except TypeError as e:
+        #         print("No games listed for",stims_ID)
+        #         continue
+        # if served_stim_ID == None:
+        #     #we haven't found the stim_ID
+        #     print("No recorded entry for game_ID in stimulus database:",gameid)
+        #     continue
+        # served_stims = stim_df.at[served_stim_ID,'stims']
+        # #let's check if we can find an entry for each stim
+        # found_empty = False
+        # for stim_ID in [s['stimulus_name'] for s in served_stims.values()]:
+        #     #check if we have an entry for that stimulus
+        #     if len(df.query("gameID == '"+gameid+"' & stimulus_name == '"+stim_ID+"'")) == 0:
+        #         found_empty = True
+        #         break
+        # if not found_empty: complete_gameids.append(gameid)
     
     # add scenario name
-    df['scenarioName'] = study.split('_')[0]
+    df['scenarioName'] = dataset
 
     # apply basic preprocessing
     df = basic_preprocessing(df)
@@ -215,9 +207,9 @@ def pull_dataframes_from_mongo(study, bucket_name, stim_version, iterationName, 
     df = df[df['gameID'].isin(complete_gameids)]
     #Generate some useful views
     df_trial_entries = df[(df['condition'] == 'prediction') & (df['trial_type'] == 'video-overlay-button-response')] #only experimental trials
-    df_trial_entries = df_trial_entries.assign(study=[study]*len(df_trial_entries), axis=0)
+    df_trial_entries = df_trial_entries.assign(study=[experiment]*len(df_trial_entries), axis=0)
     df_familiarization_entries = df[(df['condition'] == 'familiarization_prediction') & (df['trial_type'] == 'video-overlay-button-response')] #only experimental fam trials
-    df_familiarization_entries = df_familiarization_entries.assign(study=[study]*len(df_familiarization_entries), axis=0)
+    df_familiarization_entries = df_familiarization_entries.assign(study=[experiment]*len(df_familiarization_entries), axis=0)
     
     # apply anonymization
     if anonymizeIDs==True:    
