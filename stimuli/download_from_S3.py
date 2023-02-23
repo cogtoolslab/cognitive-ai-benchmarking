ca0f0a724b35c1c986181cf617fd6e78a9ec528e
Felix Binder <me@felixbinder.net>
2023-02-23 15:17:03 -0800

    helper functions to download files from S3

--- stimuli/download_from_S3.py
+++ stimuli/download_from_S3.py
@@ -29,6 +29,30 @@ def download_bucket(bucket_name, local_dir):
         bucket.download_file(obj.key, target)
     print('Downloaded all files from bucket {} to {}'.format(bucket_name, local_dir))
 
+def get_bucket_urls(bucket_name):
+    """Get all urls for files in an S3 bucket."""
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+    objects = list(bucket.objects.all())
+    urls = []
+    for obj in objects:
+        urls.append('https://s3.amazonaws.com/{}/{}'.format(bucket_name, obj.key))
+    return urls
+
+def save_url_lists(bucket_names, local_dir=None):
+    """Save lists of urls for all files in a list of S3 buckets."""
+    if local_dir is None:
+        local_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'urls')
+    # does the directory exist?
+    if not os.path.exists(local_dir):
+        os.makedirs(local_dir)
+    for bucket_name in bucket_names:
+        urls = get_bucket_urls(bucket_name)
+        with open(os.path.join(local_dir, '{}_urls.txt'.format(bucket_name)), 'w') as f:
+            for url in urls:
+                f.write('{}\n'.format(url))
+        print('Saved {} urls to {}'.format(len(urls), os.path.join(local_dir, '{}_urls.txt'.format(bucket_name))))
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Download all stimuli from S3.')
     parser.add_argument('--bucket_name', type=str, default='physion-stimuli', help='name of S3 bucket')
